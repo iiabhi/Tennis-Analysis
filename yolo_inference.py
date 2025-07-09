fca95a766318fc62702f8289b87e44c31e913dbe
iiabhi <abhishek.kumar002525@gmail.com>
2025-07-10 00:03:06 +0530

    Remove ignored files from git tracking & major changed

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,13 @@
 from ultralytics import YOLO 
 
-model = YOLO('yolov8x')
-
+# model = YOLO('yolov8x')
+model = YOLO('models/yolov5_last.pt')
+# result = model.predict("input_videos/input_video.mp4",save = True)
+# print(result)
+# print("Boxes:")
+# for box in result[0].boxes:
+#     print(box)
+# model.predict("input_videos/image.png",save = True)
 result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
 # print(result)
 # print("boxes:")
